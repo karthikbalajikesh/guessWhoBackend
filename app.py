109f4c372626c251aea4cef93bf655c9bae56573
karthik <karthik.keshavamurthi@iamrobotics.com>
2022-02-06 23:27:32 -0500

    Added player login in web server

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
-from flask import Flask
-from src.player import Player
-
-playerList = []
+from crypt import methods
+from flask import Flask, request
+import src.gameData as gameData
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/",methods=['POST','GET'])
 def index():
-    playerList.append(Player())
-    return "Player Created"
+    if request.method == 'POST':
+        gameData.generatePlayer("YOO")
+    else:
+        gameData.generatePlayer("Humble")
+    return "Hallelujah"
+
+@app.route("/login",methods=['POST'])
+def login():
+    response = {}    
+    json_data = request.json
+    playerId = gameData.generatePlayer(json_data["name"])
+    response["playerId"] = playerId
+    response["name"] = json_data["name"]
+    return json.dumps(response)
+
+
+
+@app.route("/all")
+def getListOfAllUsers():
+    nameList = []
+    for key,player in gameData.activePlayerDict.items():
+        nameList.append(player.alias)
+    return json.dumps(nameList)
+
 
 
 if (__name__ == '__main__'):
-    app.run(debug=False)
+    app.run(debug=True)
